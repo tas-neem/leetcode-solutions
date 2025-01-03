ababc12a4c70befd015aeced60f083cc9e390535
Tasneem <tasneem23122004@gmail.com>
2025-01-03 12:19:31 +0530

    Time: 2 ms (39.86%), Space: 62.9 MB (14.13%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,j=height.size()-1,area=0;
+        while(i<j){
+            area=max(area,abs(j-i)*min(height[i],height[j]));
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return area;
+    }
+};
