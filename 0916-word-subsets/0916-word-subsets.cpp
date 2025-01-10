062d49bef1ae93a32b17309d9fa4b6904d6274a4
Tasneem <tasneem23122004@gmail.com>
2025-01-10 20:55:37 +0530

    Time: 36 ms (72.15%), Space: 84.4 MB (72.15%) - LeetHub

---
+++ 0916-word-subsets/0916-word-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> res;
+        vector<int> mp2(26,0);
+        for(string s:words2){
+            int arr[26]={0};
+            for(char c:s){
+                arr[c-'a']++;
+                mp2[c-'a']=max(mp2[c-'a'],arr[c-'a']);
+            }   
+        }
+        for(string s:words1){
+            vector<int> mp1(26,0);
+            for(char c:s){
+                mp1[c-'a']++;
+            }   
+            bool fl=true;
+            for(int i=0;i<mp1.size();i++){
+                if(mp2[i]>mp1[i])
+                    fl=false;
+            }
+            if(fl)
+                res.push_back(s);
+        }
+        return res;
+    }
+};
