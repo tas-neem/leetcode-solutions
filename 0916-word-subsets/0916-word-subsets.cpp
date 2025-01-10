fe2ee7e2cbe030e5f4f69f7f558127ae2a74ef85
Tasneem <tasneem23122004@gmail.com>
2025-01-10 20:55:48 +0530

    Time: 9 ms (92.69%), Space: 62 MB (77.17%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.cpp
+++ 0916-word-subsets/0916-word-subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> res;
-        vector<int> mp2(26,0);
+        int mp2[26]={0};
         for(string s:words2){
             int arr[26]={0};
             for(char c:s){
@@ -11,12 +11,12 @@ public:
             }   
         }
         for(string s:words1){
-            vector<int> mp1(26,0);
+            int mp1[26]={0};
             for(char c:s){
                 mp1[c-'a']++;
             }   
             bool fl=true;
-            for(int i=0;i<mp1.size();i++){
+            for(int i=0;i<26;i++){
                 if(mp2[i]>mp1[i])
                     fl=false;
             }
