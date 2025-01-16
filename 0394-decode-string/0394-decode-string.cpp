e27dfb018908253bbd32fe6d3599737a06aa1df5
Tasneem <tasneem23122004@gmail.com>
2025-01-16 18:10:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        string res="";
+        for(char c:s){
+            if(c==']'){
+                string subs="";
+                while(!st.empty() && st.top()!="["){
+                    subs+=st.top(); 
+                    st.pop();
+                }
+                reverse(subs.begin(),subs.end());
+                st.pop();
+                int count=0,place=1;
+                while(!st.empty() && isdigit(st.top()[0])){
+                    count+=(st.top()[0]-'0')*place;
+                    place*=10;
+                    st.pop();
+                }
+                string temp="";
+                while(count>0){
+                    temp+=subs;
+                    count--;
+                }
+                st.push(temp);
+            }
+            else
+                st.push(string(1,c));
+        }
+        while(!st.empty()){
+            res=st.top()+res;
+            st.pop();
+        }
+        return res;
+    }
+};
