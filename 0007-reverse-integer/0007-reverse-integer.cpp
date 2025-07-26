5d86f45e8a621208de0edb4396d1ea3f26e3954e
Tasneem <tasneem23122004@gmail.com>
2025-07-26 11:33:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.51%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,26 +2,10 @@ class Solution {
 public:
     int reverse(int x) {
         long rev=0;
-        bool isneg=false;
-        int temp,count=0;
-        if(x<0){
-            isneg=true;
-            temp=-x; 
+        while(x!=0){
+            rev=rev*10+(x%10);
+            x/=10;
         }
-        else
-            temp=x;
-        while(temp>0){
-            count++;
-            temp/=10;
-        }
-        temp=(isneg ? -x : x);
-        while(temp>0){
-            rev=rev+(temp%10*pow(10,count-1));
-            count--;
-            temp/=10;
-        }
-        if(isneg)
-            rev=-rev;
         if(rev<INT_MIN || rev>INT_MAX)
             return 0;
         return rev;
