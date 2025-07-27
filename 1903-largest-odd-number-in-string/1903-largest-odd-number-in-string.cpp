540efac004b238efab05675021a9d7361c897ea3
Tasneem <tasneem23122004@gmail.com>
2025-07-27 12:02:44 +0530

    Time: 2 ms (37.71%), Space: 19.2 MB (5.85%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int n=num.size()-1;
-        for(int i=n;i>=0;i--){
-            if((num[i]-'0') %2 != 0){
-                return num.substr(0,i+1);
+        string ans="";
+        for(int i=num.length()-1;i>=0;i--){
+            if((num[i]-'0')%2!=0){
+                while(i>=0){
+                    ans+=num[i--];
+                }
             }
         }
-        return "";
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
